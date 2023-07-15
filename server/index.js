0d5a0fe84325fd3336fe24716692cf495a97b5fb
fatiherkamdinler <fatihdinler458@gmail.com>
2023-07-16 01:22:41 +0300

    [server: #3] mongodb configuration

--- server/index.js
+++ server/index.js
@@ -5,12 +5,13 @@ import cors from 'cors'
 import dotenv from 'dotenv'
 import helmet from 'helmet'
 import morgan from 'morgan'
-import clientRoutes from './src/routes/client'
-import generalRoutes from './src/routes/general'
-import managementRoutes from './src/routes/management'
-import salesRoutes from './src/routes/sales'
+import clientRoutes from './routes/client.js'
+import generalRoutes from './routes/general.js'
+import managementRoutes from './routes/management.js'
+import salesRoutes from './routes/sales.js'
 
 dotenv.config()
+
 const app = express()
 app.use(express.json())
 app.use(helmet())
@@ -24,3 +25,13 @@ app.use('/client', clientRoutes)
 app.use('/general', generalRoutes)
 app.use('/management', managementRoutes)
 app.use('/sales', salesRoutes)
+
+const PORT = process.env.PORT || 9000
+mongoose.connect(process.env.MONGO_URL, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+}).then(() => {
+    app.listen(PORT, () => console.log(`Server is running on ${PORT}`))
+}).catch(error => {
+    console.log(error)
+})
