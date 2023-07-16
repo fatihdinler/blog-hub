f7ed2abc9cc3f5f058f135266fbab4e9105b35c0
fatiherkamdinler <fatihdinler458@gmail.com>
2023-07-16 20:29:14 +0300

    [web: #6] added routes.js file

--- client/web/src/index.js
+++ client/web/src/index.js
@@ -4,13 +4,16 @@ import './index.css'
 import { Layout } from 'app'
 import { store } from './redux/store'
 import { Provider } from 'react-redux'
+import { BrowserRouter } from 'react-router-dom'
 
 const root = ReactDOM.createRoot(document.getElementById('root'))
 
 root.render(
   <Provider store={store}>
-    <React.StrictMode>
-      <Layout />
-    </React.StrictMode>
+    <BrowserRouter>
+      <React.StrictMode>
+        <Layout />
+      </React.StrictMode>
+    </BrowserRouter>
   </Provider>
 )
