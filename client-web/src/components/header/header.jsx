456b6203538ced170c965aa06a054d14ed2892f1
fatiherkamdinler <fatihdinler458@gmail.com>
2023-09-16 18:23:11 +0300

    [client-web] worked on header component

--- client-web/src/components/header/header.jsx
+++ client-web/src/components/header/header.jsx
@@ -1,8 +1,22 @@
 import React from 'react'
+import { NavLink, Link } from 'react-router-dom'
 
 const Header = () => {
   return (
-    <div>Header</div>
+    <div>
+      <header className='header-top-strip'>
+        <div className='container-xxl'>
+          <div className='row'>
+            <div className='col-6'>
+              
+            </div>
+            <div className='col-6'>
+              
+            </div>
+          </div>
+        </div>
+      </header>
+    </div>
   )
 }
 
