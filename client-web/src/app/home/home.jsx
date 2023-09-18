ec7481f590f6aec89a1f544a885e15213d0f4e2a
fatiherkamdinler <fatihdinler458@gmail.com>
2023-09-18 20:04:41 +0300

    [client-web] added main banner component

--- client-web/src/app/home/home.jsx
+++ client-web/src/app/home/home.jsx
@@ -1,8 +1,36 @@
 import React from 'react'
+import { Image, Text } from '@chakra-ui/react'
+import { Link } from 'react-router-dom'
 
 const Home = () => {
   return (
-    <div>Home</div>
+    <div>
+      <section className='home-wrapper-primary py-5'>
+        <div className='container-xxl'>
+          <div className='row'>
+            <div className='col-6'>
+              <div className='main-banner-content p-3'>
+                <Image 
+                  src='images/main-banner-1.jpg'
+                  alt='main banner'
+                  className='img-fluid rounded-5'
+                />
+                <div className='main-banner-content'>
+                  <Text as='h4'></Text>
+                  <Text as='h5'></Text>
+                  <Text as='p'>BUY NOW</Text>
+                  <Link></Link>
+                </div>
+              </div>
+            </div>
+
+            <div className='col-6'>
+
+            </div>
+          </div>
+        </div>
+      </section>
+    </div>
   )
 }
 
