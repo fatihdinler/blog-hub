c735f00b4dec4f6ca5bf4c32a8976520cb77f832
fatiherkamdinler <fatihdinler458@gmail.com>
2023-09-19 23:25:36 +0300

    [client-web] worked on home part css

--- client-web/src/app/home/home.jsx
+++ client-web/src/app/home/home.jsx
@@ -9,17 +9,17 @@ const Home = () => {
         <div className='container-xxl'>
           <div className='row'>
             <div className='col-6'>
-              <div className='main-banner-content p-3'>
+              <div className='main-banner position-relative p-3'>
                 <Image 
                   src='images/main-banner-1.jpg'
                   alt='main banner'
                   className='img-fluid rounded-5'
                 />
-                <div className='main-banner-content'>
-                  <Text as='h4'></Text>
-                  <Text as='h5'></Text>
-                  <Text as='p'>BUY NOW</Text>
-                  <Link></Link>
+                <div className='main-banner-content position-absolute '>
+                  <Text as='h4'>SUPERCHARGED FOR PROS.</Text>
+                  <Text as='h5'>iPad S13+ pro.</Text>
+                  <Text as='p'>From $999.00 or $41.62/mo. for 24 month</Text>
+                  <Link>BUY NOW</Link>
                 </div>
               </div>
             </div>
