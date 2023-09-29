62f11a1b973fc121005fe3fa541da8a501345c88
fatiherkamdinler <fatihdinler458@gmail.com>
2023-09-29 20:17:19 +0300

    [client-web] added sign-up & forgot password pages

--- client-web/src/app/forgot-password/forgot-password.jsx
+++ client-web/src/app/forgot-password/forgot-password.jsx
@@ -1,8 +1,67 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Helmet, Breadcrumb } from '../../components'
+import { Text, FormControl, FormLabel, Input, FormHelperText, Textarea, Button } from '@chakra-ui/react'
+import { Link } from 'react-router-dom'
 
 const ForgotPassword = () => {
+  const [form, setForm] = useState({
+    email: '',
+    password: '',
+    comment: '',
+  })
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target
+    setForm({
+      ...form,
+      [name]: value,
+    })
+  }
+
   return (
-    <div>ForgotPassword</div>
+    <div>
+      <Helmet title='Reset Your Password' />
+      <Breadcrumb
+        items={[
+          { content: 'Home', link: '/', isActive: false },
+          { content: 'Reset Password', link: '/forgot-password', isActive: true },
+        ]}
+      />
+
+      <div className='forgot-password-wrapper home-wrapper-secondary py-5'>
+        <div className='container-xxl'>
+          <div className='row'>
+            <div className='col-12 d-flex justify-content-center'>
+              <div className='forgot-password-card rounded-5'>
+                <Text as='h4'>Reset Your Password</Text>
+                <Text as='p'>We'll send you an e-mail to reset your password. It'll only valid for 10 minutes.</Text>
+                <FormControl isRequired className='form-controller'>
+                  <div>
+                    <FormLabel>E-mail</FormLabel>
+                    <Input
+                      name='email'
+                      placeholder='johndoe@mail.com'
+                      value={form.email}
+                      onChange={handleInputChange}
+                    />
+                  </div>
+
+                  <div>
+                    <Button
+                      colorScheme='teal'
+                      variant='solid'
+                      className='w-100 mb-2'>
+                      Send Link
+                    </Button>
+                  </div>
+                </FormControl>
+              </div>
+
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
--- client-web/src/app/sign-up/sign-up.jsx
+++ client-web/src/app/sign-up/sign-up.jsx
@@ -1,8 +1,103 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { Helmet, Breadcrumb } from '../../components'
+import { Text, FormControl, FormLabel, Input, FormHelperText, Textarea, Button } from '@chakra-ui/react'
+import { Link } from 'react-router-dom'
 
 const SignUp = () => {
+
+  const [form, setForm] = useState({
+    firstname:'',
+    lastname: '',
+    email: '',
+    password: '',
+  })
+
+  const handleInputChange = (event) => {
+    const { name, value } = event.target
+    setForm({
+      ...form,
+      [name]: value,
+    })
+  }
+
   return (
-    <div>SignUp</div>
+    <div>
+      <Helmet title='Sign Up' />
+      <Breadcrumb
+        items={[
+          { content: 'Home', link: '/', isActive: false },
+          { content: 'Sign Up', link: '/sign-up', isActive: true },
+        ]}
+      />
+
+      <div className='sign-in-wrapper home-wrapper-secondary py-5'>
+        <div className='container-xxl'>
+          <div className='row'>
+            <div className='col-12 d-flex justify-content-center'>
+              <div className='sign-in-card rounded-5'>
+                <Text as='h4'>Sign Up</Text>
+                <FormControl isRequired className='form-controller'>
+                  <div>
+                    <FormLabel>First Name</FormLabel>
+                    <Input
+                      name='firstname'
+                      type='text'
+                      placeholder='John'
+                      value={form.firstname}
+                      onChange={handleInputChange}
+                    />
+                  </div>
+
+                  <div>
+                    <FormLabel>Last Name</FormLabel>
+                    <Input
+                      name='lastname'
+                      type='text'
+                      placeholder='Doe'
+                      value={form.lastname}
+                      onChange={handleInputChange}
+                    />
+                  </div>
+
+                  <div>
+                    <FormLabel>E-mail</FormLabel>
+                    <Input
+                      name='email'
+                      type='email'
+                      placeholder='johndoe@mail.com'
+                      value={form.email}
+                      onChange={handleInputChange}
+                    />
+                    <FormHelperText>We'll not share your e-mail address with any third users!</FormHelperText>
+                  </div>
+
+                  <div>
+                    <FormLabel>Password</FormLabel>
+                    <Input
+                      name='password'
+                      type='password'
+                      placeholder='*****'
+                      value={form.password}
+                      onChange={handleInputChange}
+                    />
+                  </div>
+
+                  <div>
+                    <Button
+                      colorScheme='teal'
+                      variant='solid'
+                      className='w-100'>
+                      Sign Up
+                    </Button>
+                  </div>
+                </FormControl>
+              </div>
+            
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
   )
 }
 
