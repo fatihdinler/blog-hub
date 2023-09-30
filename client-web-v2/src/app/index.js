d0e9161abf4bf8146a19790f57f306e2dfed7fa3
fatiherkamdinler <fatihdinler458@gmail.com>
2023-09-30 23:15:02 +0300

    [master] removed mobile and client-web-v2 projects

--- client-web-v2/src/app/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Menu } from './menu/menu'
-export { default as Home } from './home/home'
-export { default as About } from './about/about'
-export { default as Contact } from './contact/contact'
