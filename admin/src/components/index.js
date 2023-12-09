0d967071aadc95c115dc3a2f6d18e25714bfc0a2
fatiherkamdinler <fatihdinler458@gmail.com>
2023-12-09 22:56:35 +0300

    [master] recovered the project

--- admin/src/components/index.js
+++
@@ -1,3 +0,0 @@
-export { default as MainLayout } from './main-layout/main-layout'
-export { default as Input } from './input/input'
-export { default as Button } from './button/button'
--- admin/src/pages/index.js
+++
@@ -1,4 +0,0 @@
-export { default as Dashboard } from './dashboard/dashboard'
-export { default as ForgotPassword } from './forgot-password/forgot-password'
-export { default as Login } from './login/login'
-export { default as ResetPassword } from './reset-password/reset-password'
